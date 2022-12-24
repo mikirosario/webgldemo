20e24f04150aa3b1715f6175bae53ad0a711a636
Michael Rosario <mrosario@student.42madrid.com>
2022-12-24 15:35:49 +0100

    Expanded triangle class to create cube class. Made rotating cube using index of vertices to draw two rotating triangles per cube face. Instructed gl state machine not to do maths for faces at the back.

--- 3D_Cube/app.js
+++ 3D_Cube/app.js
@@ -258,6 +258,242 @@ class Triangle {
 		return this.#_matWorldMatrix;
 	}
 };
+
+class Cube {
+	constructor(gl, program)
+	{
+		this.#_gl = gl;
+		if (
+			!this.#createCubeBuffer() ||
+			!this.#createCubeAtrributePointers(program) ||
+			!this.#setVertexAttribPointers() ||
+			!this.#enableVertexAttribArrays() ||
+			!this.#createCubeUniformPointers(program) ||
+			!this.#initializeVertexUniformMatrices() //initialize uniform matrices to identity matrix
+		)
+			throw 'Cube instantiation failed.'
+	}
+
+	//Properties
+	#_gl;
+	cubeVertices = //each face consists of two triangles that share two vertices.
+	[
+		//top face
+		//x, y, z				R, G, B
+		-1.0, 1.0, -1.0,		0.5, 0.5, 0.5,
+		-1.0, 1.0, 1.0,			0.5, 0.5, 0.5,
+		1.0, 1.0, 1.0,			0.5, 0.5, 0.5,
+		1.0, 1.0, -1.0,			0.5, 0.5, 0.5,
+	
+		//left face
+		//x, y, z				R, G, B
+		-1.0, 1.0, 1.0,			0.75, 0.25, 0.5,
+		-1.0, -1.0, 1.0,		0.75, 0.25, 0.5,
+		-1.0, -1.0, -1.0,		0.75, 0.25, 0.5,
+		-1.0, 1.0, -1.0,		0.75, 0.25, 0.5,
+
+		//right face
+		//x, y, z				R, G, B
+		1.0, 1.0, 1.0,			0.25, 0.25, 0.75,
+		1.0, -1.0, 1.0,			0.25, 0.25, 0.75,
+		1.0, -1.0, -1.0,		0.25, 0.25, 0.75,
+		1.0, 1.0, -1.0,			0.25, 0.25, 0.75,
+
+		//front face
+		//x, y, z				R, G, B
+		1.0, 1.0, 1.0,			1.0, 0.0, 0.15,
+		1.0, -1.0, 1.0,			1.0, 0.0, 0.15,
+		-1.0, -1.0, 1.0,		1.0, 0.0, 0.15,
+		-1.0, 1.0, 1.0,			1.0, 0.0, 0.15,
+
+		//back face
+		//x, y, z				R, G, B
+		1.0, 1.0, -1.0,			0.0, 1.0, 0.15,
+		1.0, -1.0, -1.0,		0.0, 1.0, 0.15,
+		-1.0, -1.0, -1.0,		0.0, 1.0, 0.15,
+		-1.0, 1.0, -1.0,		0.0, 1.0, 0.15,
+
+		//bottom face
+		//x, y, z				R, G, B
+		-1.0, -1.0, -1.0,		0.5, 0.5, 1.0,
+		-1.0, -1.0, 1.0,		0.5, 0.5, 1.0,
+		1.0, -1.0, 1.0,			0.5, 0.5, 1.0,
+		1.0, -1.0, -1.0,		0.5, 0.5, 1.0
+	];
+	cubeIndices =	//which vertices form a triangle on each face
+	[
+		//top
+		0, 1, 2,
+		0, 2, 3,
+
+		//left
+		5, 4, 6,
+		6, 4, 7,
+
+		//right
+		8, 9, 10,
+		8, 10, 11,
+
+		//front
+		13, 12, 14,
+		15, 14, 12,
+		
+		//back
+		16, 17, 18,
+		16, 18, 19,
+
+		//bottom
+		21, 20, 22,
+		22, 20, 23
+	];
+	cubeVertexBuffer;
+	cubeIndexBuffer; //order of triangles to draw
+	vertCoordLocation;
+	vertColorLocation;
+	matWorldUniformLocation;
+	matViewUniformLocation;
+	matProjectionUniformLocation;
+	#_matWorldMatrix = new Float32Array(16);
+	matViewValue;
+	matProjectionValue;
+
+	//Private Methods
+	#createCubeBuffer = function ()
+	{
+		var retVal = false;
+
+		if (!(this.cubeVertexBuffer = this.gl.createBuffer()))
+			console.error('createBuffer() failed to create cubeVertexBuffer in createCubeBuffer() in Cube.constructor()');
+		else if (this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.cubeVertexBuffer))									//set cubeVertexBuffer as active buffer
+			console.error('bindBuffer() failed to bind cubeVertexBuffer in createCubeBuffer() in Cube.constructor()');
+		else if (this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.cubeVertices), this.gl.STATIC_DRAW))	//send data to active buffer (buffer type, data cast to 32 bit float, cpu->gpu direct, with no subsequent changes)
+			console.error('bufferData() failed to assign cubeVertices in createCubeBuffer() in Cube.constructor()');
+		else if (!(this.cubeIndexBuffer = this.gl.createBuffer()))
+			console.error('createBuffer() failed to create cubeIndexBuffer in createCubeBuffer() in Cube.constructor()');
+		else if (this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.cubeIndexBuffer))
+			console.error('bindBuffer() failed to bind cubeIndexBuffer in createCubeBuffer() in Cube.constructor()');
+		else if (this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.cubeIndices), this.gl.STATIC_DRAW))	//send data to active buffer (buffer type, data cast to 32 bit float, cpu->gpu direct, with no subsequent changes)
+			console.error('bufferData() failed to assign cubeIndices in createCubeBuffer() in Cube.constructor()');
+		else
+			retVal = true;
+		return retVal;
+	}
+	#createCubeAtrributePointers = function (program)
+	{
+		var retVal = false;
+
+		if ((this.vertCoordLocation = this.gl.getAttribLocation(program, 'vertPosition')) < 0)
+			console.error('getAttribLocation() failed for vertPosition in createCubeAttributePointers() in Cube.constructor()');
+		else if
+			((this.vertColorLocation = this.gl.getAttribLocation(program, 'vertColor')) < 0)
+			console.error('getAttribLocation() failed for vertColor in createCubeAttributePointers() in Cube.constructor()');
+		else
+			retVal = true;
+		return retVal;
+	}
+	#setVertexAttribPointers = function ()
+	{
+		var retVal;
+
+		try
+		{
+			this.gl.vertexAttribPointer(
+				this.vertCoordLocation,				//Attrib location in buffer
+				3,									//Number of elements in attribute
+				this.gl.FLOAT,						//Type of each element in attribute
+				this.gl.FALSE,						//Is data normalized?
+				6 * Float32Array.BYTES_PER_ELEMENT,	//Size of an individual vertex in bytes
+				0									//Offset from the beginning of a single vertex to this attribute
+			);
+		
+			this.gl.vertexAttribPointer(
+				this.vertColorLocation,				//Attrib location in buffer
+				3,									//Number of elements in attribute
+				this.gl.FLOAT,						//Type of each element in attribute
+				this.gl.FALSE,						//Is data normalized?
+				6 * Float32Array.BYTES_PER_ELEMENT,	//Size of an individual vertex in bytes
+				3 * Float32Array.BYTES_PER_ELEMENT	//Offset from the beginning of a single vertex to this attribute in bytes
+			);
+			retVal = true;
+		}
+		catch (e)
+		{
+			retVal = false;
+			console.error('vertexAttribPointer() failed in setVertexAttribPointers in Cube.constructor.\n', e);
+		}
+		return retVal;
+	}
+	#enableVertexAttribArrays = function ()
+	{
+		var retVal = false;
+		this.gl.enableVertexAttribArray(this.vertCoordLocation);	//enable attribute for use
+		this.gl.enableVertexAttribArray(this.vertColorLocation);	//enable attribute for use
+		if (this.gl.getError() == this.gl.NO_ERROR)
+			retVal = true;
+		else
+			console.error('vertexAttribArray() failed in enableVertexAttribArrays() in Cube.constructor');
+		return retVal;
+	}
+	#createCubeUniformPointers = function (program)
+	{
+		var retVal = false;
+		if ((this.matWorldUniformLocation = this.gl.getUniformLocation(program, 'mWorld')) == null)
+			console.error('getUniformLocation() failed for mWorld in createCubeAttributePointers() in Cube.constructor()');
+		else if ((this.matViewUniformLocation = this.gl.getUniformLocation(program, 'mView')) == null)
+			console.error('getUniformLocation() failed for mView in createCubeAttributePointers() in Cube.constructor()');
+		else if ((this.matProjectionUniformLocation = this.gl.getUniformLocation(program, 'mProjection')) == null)
+			console.error('getUniformLocation() failed for mProjection in createCubeAttributePointers() in Cube.constructor()');
+		else
+			retVal = true;
+		return retVal;
+	}
+	#initializeVertexUniformMatrices = function ()
+	{
+		var retVal = false;
+		this.matViewValue = new Float32Array(16);
+		this.matProjectionValue = new Float32Array(16);
+												//camera position xyz, looking at xyz, up direction xyz (+y is up)
+		this.matViewValue = glMatrix.mat4.lookAt(this.matViewValue, [0, 0, -5], [0, 0, 0], [0, 1, 0]);
+												//vertical fov in radians, aspect ratio (viewport width/height), near bound frustum, far bound frustum
+		this.matProjectionValue = glMatrix.mat4.perspective(this.matProjectionValue, glMatrix.glMatrix.toRadian(45), canvas.width/canvas.height, 0.1, 1000.0);
+
+		this.gl.uniformMatrix4fv(this.matWorldUniformLocation, this.gl.FALSE, matIdentity);
+		//gl.uniformMatrix4fv(this.matViewUniformLocation, gl.FALSE, matIdentity);
+
+		this.gl.uniformMatrix4fv(this.matViewUniformLocation, this.gl.FALSE, this.matViewValue);
+		this.gl.uniformMatrix4fv(this.matProjectionUniformLocation, this.gl.FALSE, this.matProjectionValue);
+		if (this.gl.getError() == this.gl.NO_ERROR)
+			retVal = true;
+		else
+			console.error('uniformMatrix4fv() failed in initializeVertexUniformMatrices() in Cube.constructor');
+		return retVal;
+	}
+
+	//Public Methods
+	rotate = function()
+	{
+		//var angle = performance.now() / 1000 / 6 * 2 * Math.PI;			  //x, y, z
+			//ms since window opened / 1000 == seconds since window opened
+			//seconds since window opened / 6 == 1/6 of time since window opened
+			//2 * PI == 1 full rotation 
+			// Every sixth of the time since the window opened, a sixth of a rotation is done?
+		//console.log((performance.now() / 1000 / 6 * 2 * Math.PI) * (180.0 / Math.PI));
+		var angle = performance.now() * 0.001 * 0.06 * 2 * Math.PI;			  //x, y, z
+		glMatrix.mat4.rotate(this.#_matWorldMatrix, matIdentity, angle, [0, 1, 0]);
+		this.gl.uniformMatrix4fv(this.matWorldUniformLocation, this.gl.FALSE, this.#_matWorldMatrix);
+	}
+
+	//Getters
+	get gl()
+	{
+		return this.#_gl;
+	}
+	get matWorldMatrix()
+	{
+		return this.#_matWorldMatrix;
+	}
+};
+
 setBckrnd = function(gl)
 {
 	gl.clearColor(0.75, 0.85, 0.8, 1.0);
@@ -343,23 +579,27 @@ var InitDemo = function ()
 	//DEBUG ONLY
 
 		gl.useProgram(program); //Set active program in OpenGL state machine
-	var triangle = new Triangle(gl, program);
+	//var triangle = new Triangle(gl, program);
+	var cube = new Cube(gl, program)
 
 	setBckrnd(gl);
 
 	//Main render loop
 	//requestAnimationFrame uses 'tail recursion'.
 	//Calling itself is the last thing it does before returning, so it does not overflow the stack.
-	var angle = 0;
+	gl.enable(gl.DEPTH_TEST); //if pixel drawn on frame buffer already, only draw new pixel if it is closer to virtual camera
+	gl.enable(gl.CULL_FACE); //don't do math for culled faces
+	gl.frontFace(gl.CCW); //A face is formed by the order of the vertices appearing counter-clockwise to each other
+	gl.cullFace(gl.BACK); //Cull the faces at the back
 	var loop = function (shape)
 	{
 
 		shape.rotate();
 		setBckrnd(shape.gl);
-		shape.gl.drawArrays(shape.gl.TRIANGLES, 0, 3); //uses active buffer
+		//shape.gl.drawArrays(shape.gl.TRIANGLES, 0, 3); //uses active array buffer
+		shape.gl.drawElements(shape.gl.TRIANGLES, shape.cubeIndices.length, shape.gl.UNSIGNED_SHORT, 0);
 		requestAnimationFrame(() => loop(shape)); //ChatGPT taught me how to wrap this call in an anon function, so I can pass it with its argument :D
 	}
-
-	requestAnimationFrame(() => loop(triangle));
+	requestAnimationFrame(() => loop(cube));
 	//gl.drawArrays(gl.TRIANGLES, 0, 3); //uses active buffer
 }
